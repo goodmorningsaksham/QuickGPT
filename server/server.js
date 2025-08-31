d00e14f3b41477c42f71d567e33bbe87e596853b
goodmorningsaksham <sgupta13_be22@thapar.edu>
2025-08-31 11:06:35 +0530

    first commit

---
+++ server/server.js
@@ -0,0 +1,27 @@
+import express from 'express'
+import 'dotenv/config'
+import cors from 'cors'
+import connectDB from './configs/db.js'
+import userRouter from './routes/userRoute.js'
+import chatRouter from './routes/chatRoute.js'
+import messageRouter from './routes/messageRoutes.js'
+import creditRouter from './routes/creditRoutes.js'
+
+const app = express()
+
+await connectDB()
+
+app.use(cors())
+app.use(express.json())
+
+app.get('/', (req,res)=> res.send('Server is Live!'))
+app.use('/api/user', userRouter)
+app.use('/api/chat', chatRouter)
+app.use('/api/message', messageRouter)
+app.use('/api/credit', creditRouter)
+
+const PORT = process.env.PORT || 3000
+
+app.listen(PORT, ()=> {
+    console.log(`Server is running on port ${PORT}`)
+})
